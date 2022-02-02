8880b1798ce4db5d774cb81edff7c3370aa39788
vohoang2005 <vohoang2005@gmail.com>
2022-02-01 20:35:39 -0800

    updated

---
+++ routes/api/user-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+// GET /api/users
+router.get('/', (req, res) => {});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {});
+
+// POST /api/users
+router.post('/', (req, res) => {});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {});
+
+// GET /api/users/1
+router.get('/:id', (req, res) => {
+    User.findOne({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  // POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
