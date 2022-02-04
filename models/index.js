71cee3e1553f893f70fec999c308468e3b8e64f7
vohoang2005 <vohoang2005@gmail.com>
2022-02-03 23:03:37 -0800

    add routes

--- models/index.js
+++ models/index.js
@@ -1,14 +1,35 @@
 // import all models
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 
 // create associations
-User.hasMany(Post, {
+User.belongsToMany(Post, {
+  through: Vote,
+  as: 'voted_posts',
   foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
+Post.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_posts',
+  foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Vote.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+  foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
